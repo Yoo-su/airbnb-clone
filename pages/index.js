8613c6d5ace9fd239659b1ec8fd354438c194f03
유수현 <rhan0871@naver.com>
2021-08-15 18:56:58 +0900

    Footer 완료

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer';
 import Header from '../components/Header'
 import LargeCard from '../components/LargeCard';
 import MediumCard from '../components/MediumCard';
@@ -52,14 +53,13 @@ export default function Home({ exploreData, cardsData }) {
           />
       </main>
 
-      <footer>
-
-      </footer>
+      <Footer />
+  
     </div>
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const exploreData = await fetch('https://links.papareact.com/pyp').then(res => res.json());
 
   const cardsData = await fetch('https://links.papareact.com/zp1').
