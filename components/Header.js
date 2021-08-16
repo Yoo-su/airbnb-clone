979f26f15ce1338d8f24108db61eb9912912a205
유수현 <rhan0871@naver.com>
2021-08-16 16:52:21 +0900

    검색창 placeholder를 props로 받도록 수정

--- components/Header.js
+++ components/Header.js
@@ -10,12 +10,14 @@ import {
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import {DateRangePicker} from 'react-date-range';
+import { useRouter } from 'next/dist/client/router';
 
-function Header() {
+function Header({placeholder}) {
     const [searchInput, setSearchInput] = useState('');
     const [startDate, setStartDate]=useState(new Date());
     const [endDate, setEndDate]=useState(new Date());
     const [noOfGuests, setNoOfGuests]=useState(1);
+    const router=useRouter()
     
     const handleSelect=(ranges)=>{
         setStartDate(ranges.selection.startDate);
@@ -32,12 +34,26 @@ function Header() {
         key:'selection'
     }
 
+    const search=()=>{
+        router.push({
+            pathname:'/search',
+            query:{
+                location:searchInput,
+                startDate:startDate.toISOString(),
+                endDate:endDate.toISOString(),
+                noOfGuests
+            }
+        })
+    }
+
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white
         shadow-md md:px-10 p-5'>
             {/*Left*/}
             <div className='relative flex items-center h-10 cursor-pointer
-            my-auto'>
+            my-auto'
+            onClick={()=>router.push('/')}
+            >
                 <Image
                     src='https://links.papareact.com/qd3'
                     layout='fill'
@@ -52,7 +68,7 @@ function Header() {
                 <input
                     className='outline-none bg-transparent pl-5 flex-grow
                                     text-sm text-gray-600 placeholder-gray-400'
-                    placeholder='Start your search'
+                    placeholder={placeholder || 'Start your search'}
                     value={searchInput}
                     onChange={(e)=>setSearchInput(e.target.value)}
                     />
@@ -93,7 +109,9 @@ function Header() {
                     <div className='flex'>
                         <button className='flex-grow text-gray-500'
                          onClick={resetInput}>Cancle</button>
-                        <button className='flex-grow text-red-400'>Search</button>
+                        <button 
+                            onClick={search}
+                            className='flex-grow text-red-400'>Search</button>
                     </div>
                 </div>
             )}
