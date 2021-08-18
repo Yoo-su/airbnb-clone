c0bf4617f22c1aac87382c7a069557d5dd74b346
유수현 <rhan0871@naver.com>
2021-08-18 16:47:17 +0900

    -

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports={
     images:{
         domains:["links.papareact.com"]
+    },
+    env:{
+        mapbox_key:'pk.eyJ1IjoiYm9uYWpveSIsImEiOiJja3NneHM3NXgxcDBoMnFvbW12cjRqczl4In0.o7nlr_rk9Z0JtE1cxX-4eQ'
     }
 }
